624311bedce745d6d6b63191fa97307d6658841c
clawilso15 <clawilso15@gmail.com>
2020-03-02 09:49:24 -0500

    Updated Description file and added error checking to create_corpus

--- R/create_corpus.R
+++ R/create_corpus.R
@@ -5,14 +5,32 @@
 #' @importFrom ggplot2 sym
 #' @importFrom quanteda corpus docvars
 #' 
+#' @param df A data frame containing header values and single comments column
+#' @param text_col A \code{string} value identifying column with text to analyze
+#' 
 #' @return A \code{quanteda} corpus object 
 #' @export
 create_corpus <- function(df, text_col) {
   
+  #Error check provided arguments
+  if(!is.data.frame(df)){
+    stop('df must be a data frame')
+  } 
+    
+  if(!(text_col %in% colnames(df))) {
+    
+    #Need to fix column name output readability
+    stop('This function requires that the text_col be a data frame header.\n',
+         colnames(df))
+  }
+    
+  #Create a quanteda corpus from identified text column
   my_corpus <- quanteda::corpus(df[[text_col]])
   
+  #Create temprary data frame without text column
   other_cols = colnames(df)[!(colnames(df) %in% text_col)]
   
+  #Create quanteda document variables
   for(i in other_cols){
     
       quanteda::docvars(my_corpus, i) <- df[[i]]
