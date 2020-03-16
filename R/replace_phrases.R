21af0de4626d076b7ef12071acef809c7cd32385
clawilso15 <clawilso15@gmail.com>
2020-03-15 21:04:53 -0400

    Added basic documentation skeleton to replace phrases function and updated description imports

--- R/replace_phrases.R
+++ R/replace_phrases.R
@@ -1,3 +1,13 @@
+#' Replace text values
+#'
+#' @param text 
+#' @param old 
+#' @param new 
+#' 
+#' @importFrom stringr str_replace_all
+#'
+#' @return
+#'
 replace_phrases <- function(text, old, new){
   
   stringr::str_replace_all(string = text, 
