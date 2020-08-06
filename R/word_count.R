5d797e37de7e5c8e731c73a202420a8e6acdc505
Auburngrads <Auburngrads@live.com>
2020-08-06 14:20:00 -0400

    Update imports

--- R/word_count.R
+++ R/word_count.R
@@ -9,6 +9,8 @@
 #'        \code{.data} is a character vector the object returned will be a two-column
 #'        \code{data.frame} in which the first column contains the text from each document
 #'        and the second column will contain
+#'
+#' @importFrom stringr str_count str_match_all
 #' @export
 word_count <- function(.data, .column = NULL){
   
@@ -30,12 +32,12 @@ funs <-
     function(s) sapply(gregexpr("[[:alpha:]]+", s), function(x) sum(x > 0)),
     function(s) vapply(strsplit(s, "\\W+"), length, integer(1)),
     function(s) length(strsplit(gsub(' {2,}', ' ', s), ' ')[[1]]),
-    function(s) length(str_match_all(s, "\\S+")[[1]]),
+    function(s) length(stringr::str_match_all(s, "\\S+")[[1]]),
     function(s) str_count(s, "\\S+"),
     function(s) sapply(gregexpr("\\W+", s), function(x) sum(x > 0)) + 1,
     function(s) length(unlist(strsplit(s," "))),
     function(s) sapply(strsplit(s, " "), length),
-    function(s) str_count(s, '\\w+')
+    function(s) stringr::str_count(s, '\\w+')
   )
 
   words = lapply(funs, score)
